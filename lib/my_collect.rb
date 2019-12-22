eadb1919617be5b6776a18a83427e5edd337fdd6
Ricky dildy <rdildy92@gmail.com>
2019-12-22 22:26:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,10 @@ def my_collect(students)
   students.each do |student|
     student.split(" ").first 
     while i < languages.length 
-    yield(languages[i])
+    yield(languages[i]).upcase 
   end 
   students
+end 
+    languages
 end
 
