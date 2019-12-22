25fe610000993660cf0e1bf3a0833d20c59dabe9
Ricky dildy <rdildy92@gmail.com>
2019-12-22 22:36:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,15 +1,8 @@
-
-
-
-
-
-
-
-# def my_collect(students)
-#     my_collect.each do |student|
-#     student.split(" ").first 
-#     puts students
-#   end 
-#   students
-# end 
+def my_collect(students)
+    my_collect.each do |student|
+    student.split(" ").first 
+    puts students
+  end 
+  students
+end 
 
