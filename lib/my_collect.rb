0d2a6a6d3944b0d28ebd0221a49c0b2684b84ced
Ricky dildy <rdildy92@gmail.com>
2019-12-22 22:43:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,11 @@
 def my_collect(students)
-    my_collect.each do |name|
-    name.split(" ").first 
-  end 
-end 
+     i = 0 
+     name_collection = []
+while i < students.length
+    name_collection.push yield(students[i])
+    i += 1 
+end     
+  name_collection
+end
 
+my_collect(list) {|i| i.split(" ").first}
