ccd79c86cc104d0a152ab668a1a8b2dd5143a14e
Ricky dildy <rdildy92@gmail.com>
2019-12-22 22:22:16 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,6 @@
 def my_collect(students)
   students.each do |student|
-    i = 0 
-    while i < students.length 
-      studentsi << yield(students[i])
-      i += 1 
+    
   end 
   students
 end
